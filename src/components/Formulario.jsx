3409933743b484a5aa7f9cb69c761a43615a67bd
Luis Vazquez <luvazpa@gmail.com>
2022-04-22 16:52:15 -0500

    Default prop added, edit client funct added

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,12 @@
 import { Field, Form, Formik } from 'formik'
+import { useNavigate } from 'react-router-dom'
 import * as Yup from 'yup'
 
 import Alerta from './Alerta'
 
-const Formulario = () => {
+const Formulario = ({ cliente }) => {
+  const navigate = useNavigate()
+
   const nuevoClienteSchema = Yup.object().shape({
     nombre: Yup.string()
       .min(3, 'El nombre es muy corto')
@@ -20,23 +23,51 @@ const Formulario = () => {
     notas: Yup.string()
   })
 
+  const handleSubmit = async (values, { resetForm }) => {
+    try {
+      if (cliente.id) {
+        const url = `http://localhost:4000/clientes/${cliente.id}`
+
+        await window.fetch(url, {
+          method: 'PUT',
+          body: JSON.stringify(values),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+      } else {
+        const url = 'http://localhost:4000/clientes'
+
+        await window.fetch(url, {
+          method: 'POST',
+          body: JSON.stringify(values),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+      }
+      resetForm()
+      navigate('/clientes')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className='mt-10 bg-white px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto'>
       <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>
-        Agregar cliente
+        {cliente?.nombre ? 'Editar Cliente' : 'Agregar cliente '}
       </h1>
       <Formik
         initialValues={{
-          nombre: '',
-          empresa: '',
-          email: '',
-          telefono: '',
-          notas: ''
+          nombre: cliente?.nombre ?? '',
+          empresa: cliente?.empresa ?? '',
+          email: cliente?.email ?? '',
+          telefono: cliente?.telefono ?? '',
+          notas: cliente?.notas ?? ''
         }}
-        onSubmit={(values) => {
-          console.log('enviando formulario')
-          console.log(values)
-        }}
+        enableReinitialize
+        onSubmit={handleSubmit}
         validationSchema={nuevoClienteSchema}
       >
         {({ errors, touched }) => (
@@ -114,7 +145,7 @@ const Formulario = () => {
             </div>
             <input
               type='submit'
-              value='Agregar Cliente'
+              value={cliente?.nombre ? 'Editar Cliente' : 'Agregar cliente '}
               className='mt-5 w-full bg-blue-800 p-3 text-white font-bold uppercase text-lg'
             />
           </Form>
@@ -122,6 +153,10 @@ const Formulario = () => {
       </Formik>
     </div>
   )
+}
+
+Formulario.defaultProps = {
+  cliente: {}
 }
 
 export default Formulario
